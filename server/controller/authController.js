33b158b852e04e912689c3bfaa10f736adddc6f8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-28 06:08:38 +0530

    Update

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -4,9 +4,41 @@ const bcrypt = require('bcrypt');
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto');
+const Role = require('../model/Role');
 
 
 const authController = {
+
+    createPermissions: async(req, res) => {
+        try{
+            const {
+                role,
+                permission,
+            } = req.body
+
+            const checkpermaission = await Role.findOne({ name:  role })
+
+            if(!checkpermaission){
+                const newPermission = new Role({
+                    name: role,
+                    permissions: 'defult_permissions'
+                })
+
+                const resultNewPermission = await newPermission.save()
+
+                if(resultNewPermission){
+                    return res.json({ Status: "Success", Message: "New Role Created and set as Defult Permissions"})
+                }
+            }
+
+            
+
+
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 };
 
 module.exports = authController;
