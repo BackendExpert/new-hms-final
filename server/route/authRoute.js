088f6ed8692094a5678c9d2ee82ab99fd6c7449d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-28 07:27:54 +0530

    Update authRoute.js

--- server/route/authRoute.js
+++ server/route/authRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post('/signup', authController.signup)
 router.post('/verify-email', authController.otpverifyforemail)
 router.post('/signin', authController.signin)
+
+// admin, director access only
 router.post('/create-permission', authController.createPermissions)
 
 module.exports = router;
