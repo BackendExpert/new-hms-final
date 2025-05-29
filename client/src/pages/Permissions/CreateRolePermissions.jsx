845b0959bbc4c5f0a437a61151a7c3fc9d6d1d5c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-29 14:21:03 +0530

    Update CreateRolePermissions.jsx

--- client/src/pages/Permissions/CreateRolePermissions.jsx
+++ client/src/pages/Permissions/CreateRolePermissions.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DefaultBtn from '../../components/Buttons/DefaultBtn'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
+import axios from 'axios'
 
 const CreateRolePermissions = () => {
+    const navigate = useNavigate()
+    const token = secureLocalStorage.getItem('login')
+    const [permissiondata, setpermissiondata] = useState({
+        role: "",
+        permission: "",
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setpermissiondata(prev => ({ ...prev, [name]: value }));
+    };
+
+    const headleCreatePermission = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(import.meta.env.VITE_APP_API + '/auth/create-permission', permissiondata, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+            if(res.data.Status === "Success"){
+                alert(res.data.Message)
+                navigate('/Dashboard/Permissions', {replace: true })
+            }
+            else{
+                alert(res.data.Error)
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+
     return (
         <div>
             <h1 className="font-bold text-emerald-600 text-xl">Create Role and Permissions</h1>
