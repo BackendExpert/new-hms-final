be7172b9b12534f4e6dfc663126f8c1422b251c4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-29 12:40:54 +0530

    Update

--- server/route/authRoute.js
+++ server/route/authRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const authController = require('../controller/authController');
+const { authMiddleware } = require('../middlewares/AuthMiddleware');
+const checkPermission = require('../middlewares/checkPermissionMiddleware');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup)
+
 router.post('/verify-email', authController.otpverifyforemail)
 
 router.post('/signin', authController.signin)
@@ -15,6 +18,11 @@ router.post('/verify-otp', authController.checkotpforforgetpass)
 router.post('/update-password', authController.updatepasswordviaforgetpass)
 
 // admin, director access only
-router.post('/create-permission', authController.createPermissions)
+router.post('/create-permission', authMiddleware, checkPermission(['create-role-permission']), authController.createPermissions)
+
+router.post('/view-all-role', authMiddleware, checkPermission(['view-role-permission']), authController.getallrolesWithPermissions)
+
+router.post('/view-one-role', authMiddleware, checkPermission(['view-one-role-permission']), authController.viewoneROleWithPermissions)
+
 
 module.exports = router;
