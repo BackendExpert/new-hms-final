095177c3f277fcf8344af2cd5b3d9489f57ae429
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-30 21:20:44 +0530

    Update studentRoute.js

--- server/route/studentRoute.js
+++ server/route/studentRoute.js
@@ -8,4 +8,6 @@ const router = express.Router();
 
 router.post('/create-student-sheet', authMiddleware, upload.single('file'), checkPermission('create-student-sheet'), StudentController.createstdviaSheet)
 
+router.post('/create-student-manually', authMiddleware, checkPermission('create-student-manually'), StudentController.createStudentManually)
+
 module.exports = router;
