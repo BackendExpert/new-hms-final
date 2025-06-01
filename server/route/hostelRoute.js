41d0c33fd98734af8704343b76d07bb7015e4633
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-01 18:59:29 +0530

    Update hostelRoute.js

--- server/route/hostelRoute.js
+++ server/route/hostelRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const HostelController = require('../controller/hostelController');
+const { authMiddleware } = require('../middlewares/AuthMiddleware');
+const checkPermission = require('../middlewares/checkPermissionMiddleware');
 
 const router = express.Router();
 
-router.get('/get-all-warden', HostelController.GetallWarden)
+router.get('/get-all-warden', authMiddleware, checkPermission('get-all-warden'), HostelController.GetallWarden)
+
+router.post('/create-hostel', authMiddleware, checkPermission('create-hostel'), HostelController.CreateHostel)
 
 module.exports = router;
