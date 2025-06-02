4d16a852ae5506be307120c35fdea342e21fffe1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-06-02 07:16:28 +0530

    Update ViewRoom.jsx

--- client/src/pages/Rooms/ViewRoom.jsx
+++ client/src/pages/Rooms/ViewRoom.jsx
@@ -20,23 +20,33 @@ const ViewRoom = () => {
             .catch(err => console.log(err))
     }, [])
     return (
-        <div className="p-6 max-w-4xl mx-auto bg-white shadow-md rounded-lg mt-5">
-            <div className="-mt-4 mb-2">
-                <Link to={'/Dashboard/Rooms'}>
-                    <DefaultBtn type="button" label="Back" />
-                </Link>
+        <div className="">
+            <div className="p-6 max-w-4xl mx-auto bg-white shadow-md rounded-lg mt-5">
+                <div className="-mt-4 mb-2">
+                    <Link to={'/Dashboard/Rooms'}>
+                        <DefaultBtn type="button" label="Back" />
+                    </Link>
+                </div>
+
+                <h1 className="text-2xl font-bold text-emerald-700 mb-4">Room Information: {oneRoom?.roomID}</h1>
+
+                <div className="">
+                    <div className="grid md:grid-cols-3 gap-4 text-sm">
+                        <Detail label={"Room ID"} value={oneRoom?.roomID} />
+                        <Detail label={"Room Current Occupants"} value={oneRoom?.currentOccupants} />
+                        <Detail label={"Room Capasity"} value={oneRoom?.capasity} />
+                        <Detail label={"Room Gender"} value={oneRoom?.gender} />
+                        <Detail label={"Room ID"} value={oneRoom?.hostelID?.name} />
+                    </div>
+                </div>
             </div>
 
-            <h1 className="text-2xl font-bold text-emerald-700 mb-4">Room Information: {oneRoom?.roomID}</h1>
+            <div className="p-6 max-w-4xl mx-auto bg-white shadow-md rounded-lg mt-5">
+                <h1 className="text-2xl font-bold text-emerald-700 mb-4">Update in Room Capasity</h1>
+            </div>
 
-            <div className="">
-                <div className="grid md:grid-cols-3 gap-4 text-sm">
-                    <Detail label={"Room ID"} value={oneRoom?.roomID} />
-                    <Detail label={"Room Current Occupants"} value={oneRoom?.currentOccupants} />
-                    <Detail label={"Room Capasity"} value={oneRoom?.capasity} />
-                    <Detail label={"Room Gender"} value={oneRoom?.gender} />
-                    <Detail label={"Room ID"} value={oneRoom?.hostelID?.name} />
-                </div>
+            <div className="p-6 max-w-4xl mx-auto bg-white shadow-md rounded-lg mt-5">
+                <h1 className="text-2xl font-bold text-emerald-700 mb-4">Students in Room</h1>
             </div>
         </div>
     )
