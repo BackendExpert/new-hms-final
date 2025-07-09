1b0d3fd60ce491bc050d7451f29be5d51321550a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-09 20:58:50 +0530

    Update StdExtraNeeds.jsx

--- client/src/pages/StdExtraNeeds/StdExtraNeeds.jsx
+++ client/src/pages/StdExtraNeeds/StdExtraNeeds.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const StdExtraNeeds = () => {
+    const [getextraneeds, setgetextraneeds] = useState([])
+
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_APP_API + '/warden/std-extra-needs', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(res => setgetextraneeds(res.data.Result))
+        .catch(err => console.log(err))
+    }, [])
+    
     return (
         <div>StdExtraNeeds</div>
     )
